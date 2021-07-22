5d50740480a265ff980e666f311688c06716febe
Noma <wiostz98kr@gmail.com>
2021-07-22 22:57:25 +0900

    Implement home page for logged-in user

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,9 +1,38 @@
+import { useHistory } from 'react-router-dom';
+import Post from '../../components/Post/post';
 import withAuth from '../../hoc/withAuth'
+import styles from './home.module.css';
 
 function Home({ user }) {
+    const history = useHistory();
+    const handleCreate = () => {
+        history.push("/post/create");
+    };
     return (
-        <div>
-            {user ? `Hello, ${user.name}!` : "This is home..."}
+        <div className={styles.home}>
+            {user ? (
+                <>
+                    <h2>{user.blogInfo.blogName}</h2>
+                    <div className={styles.user}>
+                        <img className={styles.avatar} src={user.avatar || "/images/default.PNG"} alt="user avatar" />
+                        <div className={styles.info}>
+                            <h3>{user.name}</h3>
+                            <p>Hi, I'm noma! I like pizza 🍕</p>
+                        </div>
+                    </div>
+                    <div className={styles.latest}>
+                        <div className={styles.latestHeader}>
+                            <h2>Latest Posts</h2>
+                            <button onClick={handleCreate}><i className="fas fa-edit"></i></button>
+                        </div>
+                        <Post />
+                    </div>
+                </>
+            ) : (
+                <>
+                    <h2>Log in and write your own writing!</h2>
+                </>
+            )}
         </div>
     )
 }
