5f07f9ba5d59da839996245388f97d180eb96294
Noma <wiostz98kr@gmail.com>
2021-08-08 16:33:40 +0900

    Change NotFound to ErrorPage to show proper message according to status code

---
+++ src/pages/ErrorPage/errorPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import styles from './errorPage.module.css';
+
+function ErrorPage({ statusCode }) {
+    return (
+        <div className={styles.notFound}>
+            <h1>{statusCode === "404" ? "404 Not Found" : "403 Forbidden"}</h1>
+            <p>{`${statusCode === "404" ? "올바르지 않은 경로입니다." : "접근할 수 있는 권한이 없습니다."} 홈 화면으로 돌아가세요.`}</p>
+            <Link to="/">Go back to home →</Link>
+        </div>
+    )
+}
+
+export default ErrorPage
