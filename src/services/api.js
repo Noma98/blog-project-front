a03720f7e73c58abbaf704eafb23f5d040513771
Noma <wiostz98kr@gmail.com>
2021-08-18 15:03:34 +0900

    Use axios instance to avoid setting url and config repeatedly

--- src/services/api.js
+++ src/services/api.js
@@ -1,72 +1,84 @@
 import axios from 'axios';
-const API_URL = process.env.REACT_APP_API_URL;
+const userAPI = axios.create({
+    baseURL: process.env.REACT_APP_API_URL + "/api/users",
+    withCredentials: true
+})
+const folderAPI = axios.create({
+    baseURL: process.env.REACT_APP_API_URL + "/api/folders",
+    withCredentials: true
+})
+const postAPI = axios.create({
+    baseURL: process.env.REACT_APP_API_URL + "/api/posts",
+    withCredentials: true
+})
+
 class Api {
     //-------------User------------------
     async join(data) {
-        const response = await axios.post(`${API_URL}/api/users/join`, data);
+        const response = await userAPI.post('join', data);
         return response.data;
     }
     async socialJoin(data) {
-        const response = await axios.post(`${API_URL}/api/users/join/social`, data);
+        const response = await userAPI.post('join/social', data);
         return response.data;
     }
     async login(data) {
-        const response = await axios.post(`${API_URL}/api/users/login`, data, { withCredentials: true, credentials: 'include' });
+        const response = await userAPI.post('login', data);
         return response.data;
     }
     async logout() {
-        const response = await axios.get(`${API_URL}/api/users/logout`, { withCredentials: true, credentials: 'include' });
+        const response = await userAPI.get('logout');
         return response.data;
     }
     async loginGithub(code) {
-        const response = await axios.post(`${API_URL}/api/users/github`, { code }, { withCredentials: true, credentials: 'include' });
+        const response = await userAPI.post('github', { code });
         return response.data;
     }
     async loginKakao(code) {
-        const response = await axios.post(`${API_URL}/api/users/kakao`, { code }, { withCredentials: true, credentials: 'include' });
+        const response = await userAPI.post('kakao', { code });
         return response.data;
     }
     async kakaoUnlink(accessToken) {
-        const response = await axios.post(`${API_URL}/api/users/kakao/unlink`, { accessToken });
+        const response = await userAPI.post('kakao/unlink', { accessToken });
         return response.data;
     }
     async googleLogin(userData) {
-        const response = await axios.post(`${API_URL}/api/users/google`, userData, { withCredentials: true, credentials: 'include' });
+        const response = await userAPI.post('google', userData);
         return response.data;
     }
     async naverLogin(token) {
-        const response = await axios.post(`${API_URL}/api/users/naver`, { token }, { withCredentials: true, credentials: 'include' });
+        const response = await userAPI.post('naver', { token });
         return response.data;
     }
     async naverUnlink(token) {
-        const response = await axios.post(`${API_URL}/api/users/naver/unlink`, { token });
+        const response = await userAPI.post('naver/unlink', { token });
         return response.data;
     }
     async updatePwd(data) {
-        const response = await axios.post(`${API_URL}/api/users/update/password`, data, { withCredentials: true });
+        const response = await userAPI.post('update/password', data);
         return response.data;
     }
     async updateUser(data) {
-        const response = await axios.post(`${API_URL}/api/users/update`, data, { withCredentials: true });
+        const response = await userAPI.post('update', data);
         return response.data;
     }
     async deleteUser() {
-        const response = await axios.get(`${API_URL}/api/users/delete`, { withCredentials: true });
+        const response = await userAPI.get('delete');
         return response.data;
     }
     async updateBlogInfo(data) {
-        const response = await axios.post(`${API_URL}/api/users/update/blog`, data, { withCredentials: true });
+        const response = await userAPI.post('update/blog', data);
         return response.data;
     }
     async fetchPublicUser(nickname) {
-        const response = await axios.post(`${API_URL}/api/users/public`, { nickname });
+        const response = await userAPI.post('public', { nickname });
         if (!response.data.success) {
             return null;
         }
         return response.data.payload;
     }
     async fetchLoginUser() {
-        const response = await axios.get(`${API_URL}/api/users/auth`, {
+        const response = await userAPI.get('auth', {
             withCredentials: true
         });
         if (!response.data.success) {
@@ -77,25 +89,25 @@ class Api {
     }
     //------------------Folder---------------------
     async makeFolder() {
-        const response = await axios.get(`${API_URL}/api/folders/create`, { withCredentials: true });
+        const response = await folderAPI.get('create');
         if (response.data.success) {
             return;
         }
         alert("폴더 생성에 실패하였습니다.")
     }
     async editFolder(folderId, newName) {
-        const response = await axios.post(`${API_URL}/api/folders/edit`, {
+        const response = await folderAPI.post('edit', {
             folderId, newName
-        }, { withCredentials: true });
+        });
         if (response.data.success) {
             return;
         }
         alert("변경 요청에 실패하였습니다.")
     }
     async deleteFolder(folderId, userId) {
-        const response = await axios.post(`${API_URL}/api/folders/delete`, {
+        const response = await folderAPI.post('delete', {
             folderId, userId
-        }, { withCredentials: true });
+        });
         if (response.data.success) {
             return;
         }
@@ -103,64 +115,64 @@ class Api {
     }
     //-----------------Post----------------------
     async createNewPost(data) {
-        const response = await axios.post(`${API_URL}/api/posts/create`, data, { withCredentials: true });
+        const response = await postAPI.post('create', data);
         if (response.data.success) {
             return;
         }
         alert("게시글 생성에 실패하였습니다.")
     }
     async updatePost(data) {
-        const response = await axios.post(`${API_URL}/api/posts/update`, data, { withCredentials: true });
+        const response = await postAPI.post('update', data);
         if (response.data.success) {
             return;
         }
         alert("게시글 변경에 실패하였습니다.");
     }
     async deletePost(postId, folderId) {
-        const response = await axios.post(`${API_URL}/api/posts/delete`, { postId, folderId }, { withCredentials: true });
+        const response = await postAPI.post('delete', { postId, folderId });
         if (response.data.success) {
             return;
         }
         alert("게시글 삭제 요청에 실패하였습니다.");
     }
     async fetchPosts(folderId) {
-        const response = await axios.post(`${API_URL}/api/posts/read`, { folderId });
+        const response = await postAPI.post('read', { folderId });
         return response.data;
     }
     async fetchPostDetail(postId) {
-        const response = await axios.post(`${API_URL}/api/posts/detail`, { postId });
+        const response = await postAPI.post('detail', { postId });
         if (response.data.success) {
             return response.data.payload;
         }
         alert("게시글을 불러오는 데 실패하였습니다.");
     }
     async fetchUserPosts(userId) {
-        const response = await axios.post(`${API_URL}/api/posts/user`, { userId });
+        const response = await postAPI.post('user', { userId });
         if (response.data.success) {
             return response.data.payload;
         }
         return alert("게시글을 불러오는 데 실패하였습니다.");
     }
     async fetchResults(query, userId) {
-        const response = await axios.post(`${API_URL}/api/posts/search`, { query, userId });
+        const response = await postAPI.post('search', { query, userId });
         if (response.data.success) {
             return response.data.payload;
         }
         return alert("게시글 검색에 실패하였습니다.");
     }
     async fetchLatest(userId) {
-        const response = await axios.post(`${API_URL}/api/posts/latest`, { userId });
+        const response = await postAPI.post('latest', { userId });
         if (response.data.success) {
             return response.data.payload;
         }
         return alert("최신 글을 불러오는 데 실패하였습니다.");
     }
     async fetchAllPosts() {
-        const response = await axios.get(`${API_URL}/api/posts/all`);
+        const response = await postAPI.get('all');
         return response.data;
     }
     async fetchAllResults(query) {
-        const response = await axios.post(`${API_URL}/api/posts/search/global`, { query });
+        const response = await postAPI.post('search/global', { query });
         return response.data;
     }
 }
