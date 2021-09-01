de1d00611ccac4ad1df217356201202a25cfb453
Noma <wiostz98kr@gmail.com>
2021-09-01 10:34:16 +0900

    Make Weather render appropriately for variety of situations

--- src/components/Weather/weather.jsx
+++ src/components/Weather/weather.jsx
@@ -1,19 +1,21 @@
 import React, { memo, useCallback, useEffect, useState } from 'react'
+import Loading from '../Loading/loading';
 import styles from './weather.module.css';
 
 const Weather = memo(({ api }) => {
     const [weather, setWeather] = useState();
+
     const onGeoSuccess = async (position) => {
         const coords = position.coords;
         window.sessionStorage.setItem('coords', JSON.stringify({ lat: coords.latitude, lon: coords.longitude }));
     }
     const onGeoError = () => {
-        alert("위치 접근에 허용하지 않으면, 현재 날씨 제공이 어렵습니다.");
+        setWeather({ location: '🧚‍♀️: 위치를 찾을 수 없습니다. 날씨 제공을 원한다면, 추적에 허용하고 새로고침 해주세요.' });
         window.sessionStorage.setItem('coords', JSON.stringify({ lat: undefined, lon: undefined }));
     }
     const getWeather = useCallback(async () => {
         const coords = JSON.parse(window.sessionStorage.getItem('coords'));
-        if (coords.lat !== undefined) {
+        if (coords?.lat !== undefined) {
             const { lat, lon } = coords;
             const res = await api.getWeather(lat, lon);
             const {
@@ -26,33 +28,29 @@ const Weather = memo(({ api }) => {
             setWeather({ location, temp: temp.toFixed(1), icon: weather[0].icon });
         }
     }, [api]);
-    useEffect(() => {
-        getWeather();
-        const id = setInterval(() => {
-            getWeather();
-        }, 1000 * 60 * 60);
-        return () => clearInterval(id);
-    }, [getWeather]);
+
+    const getCoords = useCallback(() => {
+        navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError);
+    }, []);
 
     useEffect(() => {
-        if (!window.sessionStorage.getItem('coords')) {
-            navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError);
-        }
-    }, []);
+        getCoords();
+        getWeather();
+    }, [getWeather, getCoords]);
 
     return (
         <div className={styles.container}>
-            {weather &&
+            {weather ?
                 <>
-                    <img src={`http://openweathermap.org/img/wn/${weather.icon}@2x.png`} alt="weather" />
+                    {weather.icon && <img src={`http://openweathermap.org/img/wn/${weather.icon}@2x.png`} alt="weather" />}
                     <div className={styles.flexColumn}>
-                        <p>{`${weather.temp}℃`}</p>
-                        <p>{weather.location}</p>
+                        {weather.temp && <p>{`${weather.temp}℃`}</p>}
+                        <p className={!weather.temp && styles.msg}>{weather.location}</p>
                     </div>
-                </>
+                </> : <Loading />
+
             }
         </div>
     )
 });
-
 export default Weather
